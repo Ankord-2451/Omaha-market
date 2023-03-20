54f9c173c23e48e7003d98e116a220a135bb8aae
Ankord-2451 <107429227+Ankord-2451@users.noreply.github.com>
2023-03-20 16:54:36 +0200

    Добавьте файлы проекта.

---
+++ Omaha-market/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Omaha_market.Models;
+using System.Diagnostics;
+
+namespace Omaha_market.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Omaha-market/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Omaha_market.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
