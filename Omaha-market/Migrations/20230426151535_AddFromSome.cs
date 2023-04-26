948e3b25299b460c5893e8cf2f5989316528a8dc
Ankord-2451 <107429227+Ankord-2451@users.noreply.github.com>
2023-04-26 18:16:29 +0300

    Add Migration

---
+++ Omaha-market/Migrations/20230426151535_AddFromSome.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Omaha_market.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddFromSome : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "FromSome",
+                table: "Products",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.CreateTable(
+                name: "Email",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Email", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Email");
+
+            migrationBuilder.DropColumn(
+                name: "FromSome",
+                table: "Products");
+        }
+    }
+}
